d9fdc702b03b51d47201df39cb7da2ff64952247
samuelds <samuelds.pro@gmail.com>
2019-12-02 12:26:03 +0100

    Update to npm

---
+++ exemple/__resource.lua
@@ -0,0 +1,11 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+client_scripts {
+    -- Local files
+    "src/example.client.js",
+}
+
+server_scripts {
+    -- Local files
+    "src/example.server.js",
+}
---
+++ exemple/src/example.client.js
@@ -0,0 +1,11 @@
+//
+// @Project: FivemTools
+// @Author: Samuelds
+// @Source: https://github.com/FivemTools/ft_core
+//
+
+import {OnClientReady} from "@fivemtools/ft_core";
+
+OnClientReady(() => {
+    console.log("ft_player");
+});
